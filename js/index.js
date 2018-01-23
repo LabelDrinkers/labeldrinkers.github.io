b1c42681af6cfcc10d4a1ca09484c7464664d7b2
jurgenbuchmann <jurgenb@me.com>
2018-01-23 17:17:56 +0100

    bold fix

--- js/index.js
+++ js/index.js
@@ -78,7 +78,7 @@ var TextScramble = function () {
   return TextScramble;
 }();
 
-var phrases = ['I drink champagne when I win, to celebrate…And I drink champagne when I lose, to console myself.<br /><b>- Napoleon Bonaparte</b>','<b>Label Drinkers</b>', 'An Extraordinaire Global Series','<b>Featuring</b>','One senior Creative Head Sommelier and Decanter World Wine Awards Judge','One wannabe Drink Wizard with NameWSET Level 4 Diploma','Traveling the world, unlocking the myths, facts and legends of the most admired beverage of them all - Champagne','<b>Coming 2018</b>','When drinking is not a game.<br />But bloody serious.<br /><b>- The Authors</b>','<a href=" mailto:partnerships@labeldrinkers.com" target="_new"><b>partnerships</b>@labeldrinkers.com</a>'];
+var phrases = ['I drink champagne when I win, to celebrate…And I drink champagne when I lose, to console myself.<br />- Napoleon Bonaparte','Label Drinkers', 'An Extraordinaire Global Series','Featuring','One senior Creative Head Sommelier and Decanter World Wine Awards Judge','One wannabe Drink Wizard with NameWSET Level 4 Diploma','Traveling the world, unlocking the myths, facts and legends of the most admired beverage of them all - Champagne','Coming 2018','When drinking is not a game.<br />But bloody serious.<br />- The Authors','<a href=" mailto:partnerships@labeldrinkers.com" target="_new">partnerships@labeldrinkers.com</a>'];
 
 
 var el = document.querySelector('.text');
